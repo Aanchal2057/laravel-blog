6a03c85b6981402d42a278e660416b53bdae0a4b
Aanchal2057 <aanchalneupane@gmail.com>
2022-08-30 12:28:35 +0545

    store post

---
+++ resources/views/post.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">\
+    <link rel="stylesheet" href="app.css">
+    <title>My First Post</title>
+</head>
+<body>
+<article>
+        <!-- <h1><a href="/post">My First Post</a></h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, rerum quaerat. Illum saepe non alias aliquam, nisi quia beatae natus, assumenda ducimus minus, ea reiciendis ut tempora! Nesciunt, harum fugiat?
+            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Totam cumque consectetur accusamus ipsa obcaecati nam eveniet sint! Quod numquam est veniam rem sit in consectetur ullam, nulla odit, voluptas eligendi.
+        </p> -->
+        <?=$post; ?>
+    </article>
+    <a href="/">Go back</a>
+</body>
+</html>
---
+++ resources/views/posts.blade.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="app.css">
+    <title>My Blog</title>
+</head>
+<body>
+    <!-- <h1>Hello world!</h1> -->
+    <article>
+        <h1><a href="/post">My First Post</a></h1>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam, rerum quaerat. Illum saepe non alias aliquam, nisi quia beatae natus, assumenda ducimus minus, ea reiciendis ut tempora! Nesciunt, harum fugiat?
+        </p>
+    </article>
+    <article>
+        <h1><a href="/post"> My second blog</a></h1>
+        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Fugit error quis ex libero! Eaque quis accusamus cupiditate? Voluptas, ab corporis est quisquam dicta omnis numquam eius, illo, magni consequuntur delectus!</p>
+    </article>
+    <article>
+        <h1><a href="/post">My third blog</a></h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis architecto numquam quidem quas fuga omnis rem ipsam harum excepturi esse! Ad ipsa dolores qui ducimus, at est dolore obcaecati facere!
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, ipsam maiores recusandae ea eligendi sed minus eveniet harum! Eum possimus facere laboriosam labore id, maiores fugit alias. Veniam, ab iste!
+        </p>
+    </article>
+
+</body>
+</html>
